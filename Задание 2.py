3b49c17ba8ec031a36e7452bb5cd0890c05d8117
Paul <pavel.kosovich2004@gmail.com>
2023-10-06 09:08:49 +0300

    3 vers

--- Задание 2.py
+++ Задание 2.py
@@ -2,27 +2,20 @@ def process_input(input_data):
     if isinstance(input_data, list):
         # Найти количество четных чисел
         even_count = sum(1 for num in input_data if num % 2 == 0)
-
         # Вывести максимальное число
         max_num = max(input_data)
-
         # Удалить отрицательные элементы из списка
         input_data = [num for num in input_data if num >= 0]
-
         return f"Четные числа: {even_count}, Максимальное число: {max_num}, Список без отрицательных: {input_data}"
 
     elif isinstance(input_data, dict):
         # Отсортировать словарь по значению в порядке убывания
         sorted_dict = dict(sorted(input_data.items(), key=lambda item: item[1], reverse=True))
-
         return sorted_dict
-
     elif isinstance(input_data, int):
         # Вывести число в обратном порядке
         reversed_num = int(str(input_data)[::-1])
-
         return reversed_num
-
     elif isinstance(input_data, str):
         # Определить количество вхождений каждого символа и вывести в виде словаря
         char_count = {}
@@ -31,7 +24,6 @@ def process_input(input_data):
                 char_count[char] += 1
             else:
                 char_count[char] = 1
-
         return char_count
 
     else:
@@ -70,6 +62,8 @@ try:
             # Запрос у пользователя на ввод
             user_input = input("Введите список: ")
             input_list = [int(x) for x in user_input.split()]
+        print(
+            "Задание: найти количество четных чисел. Вывести максимальное число. Удалить все отрицательные элементы из списка.")
         print("Результат: ", process_input(input_list))
     elif a == 2:
         if choice == 1:
@@ -78,6 +72,7 @@ try:
         else:
             user_input = input("Введите данные в формате ключ1:значение1, ключ2:значение2: ")
             input_dict = dict(item.split(":") for item in user_input.split(", "))
+        print("Задание: отсортируйте по значению в порядке убывания")
         print("Результат: ", process_input(input_dict))
     elif a == 3:
         if choice == 1:
@@ -86,6 +81,7 @@ try:
         else:
             user_input = input("Введите целое число: ")
             input_num = int(user_input)
+        print("Задание: вывести в обратном порядке")
         print("Результат: ", process_input(input_num))
     elif a == 4:
         if choice == 1:
@@ -94,6 +90,7 @@ try:
         else:
             user_input = input("Введите строку: ")
             input_str = int(user_input)
+        print("Задание: определить количество вхождений каждого символа. Вывести в виде словаря")
         print("Результат: ", process_input(input_str))
     else:
         print("Неподдерживаемый тип данных")
