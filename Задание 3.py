1860e1fec57ab7c22d528780e6e48cc38868b787
Paul <pavel.kosovich2004@gmail.com>
2023-10-06 09:01:41 +0300

    3 vers

--- Задание 3.py
+++ Задание 3.py
@@ -1,12 +1,30 @@
+import random
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        row_str = " ".join(map(str, row))  # Преобразование чисел в строки и объединение элементов строки через пробел
+        print(row_str)
+
+
+def generate_random_matrix(rows, cols):
+    matrix = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            value = random.randint(-100, 100)  # Генерация случайного числа от -100 до 100
+            row.append(value)  # вставляем значение в столбец
+        matrix.append(row)  # вставляем столбцы в матрицу
+    return matrix
+
+
 def find_negative_column(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
-
     for col in range(cols):
         all_negative = True
         column_sum = 0
         count = 0
-
         for row in range(rows):
             if matrix[row][col] >= 0:
                 all_negative = False
@@ -26,17 +44,29 @@ def find_negative_column(matrix):
 
 
 def input_matrix():
+    while True:
+        try:
+            choice = int(
+                input("Хотите ввести собственные значения (нажмите 1) или сгенерировать случайные (нажмите 2)? "))
+            if choice == 1 or choice == 2:
+                break
+        except ValueError:
+            print("Неверный ввод... Повторите попытку")
     try:
-        rows = int(input("Введите количество строк матрицы: "))
-        cols = int(input("Введите количество столбцов матрицы: "))
-
-        matrix = []
-        for i in range(rows):
-            row = []
-            for j in range(cols):
-                value = int(input(f"Введите значение для [{i + 1}][{j + 1}]: "))
-                row.append(value)
-            matrix.append(row)
+        if choice == 1:
+            rows = int(input("Введите количество строк матрицы: "))
+            cols = int(input("Введите количество столбцов матрицы: "))
+            matrix = []
+            for i in range(rows):
+                row = []
+                for j in range(cols):
+                    value = int(input(f"Введите значение для [{i + 1}][{j + 1}]: "))
+                    row.append(value)
+                matrix.append(row)
+        else:
+            rows = int(input("Введите количество строк матрицы: "))
+            cols = int(input("Введите количество столбцов матрицы: "))
+            matrix = generate_random_matrix(rows, cols)
 
         return matrix
     except ValueError:
@@ -50,6 +80,7 @@ def input_matrix():
 # Пример использования
 while True:
     user_matrix = input_matrix()
+    print_matrix(user_matrix)
     if user_matrix is not None:
         column_index, average = find_negative_column(user_matrix)
         if column_index is not None:
